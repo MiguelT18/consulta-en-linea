58231bde2b13c32bfe3271756d987ab8e996618e
Miguel Teran <miguel.teranj02@gmail.com>
2023-01-05 17:39:27 -0400

    ultimos avances del proyecto de la consulta en línea

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import "./App.css";
+import { Link, Route, Routes } from "react-router-dom";
 import { Consideraciones } from "./views/Consideraciones/Consideraciones";
 import { Paso1 } from "./views/Paso1/Paso1";
 
 function App() {
   return (
-    <div className="App">
-      <Consideraciones />
-      <Paso1 />
-    </div>
+    <>
+      <Routes>
+        <Route path="/consideraciones" element={<Consideraciones />} />
+        <Route path="/formulario" element={<Paso1 />} />
+      </Routes>
+    </>
   );
 }
 
--- src/views/Consideraciones/Footer/Footer.jsx
+++ src/views/Consideraciones/Footer/Footer.jsx
@@ -1,9 +1,13 @@
 import "./css/Footer.css";
+import { Link } from "react-router-dom";
+import { Paso1 } from "../../Paso1/Paso1";
 
 export const Footer = () => {
   return (
     <footer>
-      <button>Continuar</button>
+      <Link to={<Paso1 />}>
+        <button>Continuar</button>
+      </Link>
       <p>
         <span>ADVERTENCIA:</span> Al realizar el proceso de la consulta en
         línea, tendrás un plazo de <span>siete días continuos</span>, para
