79a83fb77abdecc485695082177551c91b1e453f
Miguel Teran <miguel.teranj02@gmail.com>
2023-01-30 12:18:20 -0400

    Cambios al componente de Consideraciones (antes de hacerle git pull)

--- src/views/PagoQR/Footer/Footer.jsx
+++ src/views/PagoQR/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import "./css/Footer.css";
 export const Footer = () => {
   return (
     <footer>
-      <a href="http://wa.link/sl1i06">
+      <a href="https://wa.link/m1pjeq">
         <span></span>Quiero reportar mi pago
       </a>
       <p>También puedes consultar por todas tus dudas 👆</p>
