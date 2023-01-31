049086f62e53342152dd17e47dcde0ffd5aeea87
Miguel Teran <miguel.teranj02@gmail.com>
2023-01-31 12:25:28 -0400

    Paso1, Paso2 los hice responsive, falta hacer responsive el Paso3 y el componente de pagos QR

--- src/views/Paso2/Formulario2/Formulario2.jsx
+++ src/views/Paso2/Formulario2/Formulario2.jsx
@@ -11,105 +11,107 @@ export const Formulario2 = () => {
 
   return (
     <form className="FormuCheck">
-      <label htmlFor="check1">
-        <input
-          id="check1"
-          type="checkbox"
-          onChange={() => setIsChecked1(!isChecked1)}
-        />
-        ¿Padece alguna enfermedad?
-        <div className="Check-User-Description">
-          {isChecked1 && (
-            <textarea
-              id="TextCheck1"
-              className="TextCheck"
-              placeholder="Indícanos qué enfermedad padeces."
-              cols="40"
-              rows="3"
-            ></textarea>
-          )}
-        </div>
-      </label>
+      <div className="Checkboxes-Container">
+        <label htmlFor="check1">
+          <input
+            id="check1"
+            type="checkbox"
+            onChange={() => setIsChecked1(!isChecked1)}
+          />
+          ¿Padece alguna enfermedad?
+          <div className="Check-User-Description">
+            {isChecked1 && (
+              <textarea
+                id="TextCheck1"
+                className="TextCheck"
+                placeholder="Indícanos qué enfermedad padeces."
+                cols="40"
+                rows="3"
+              ></textarea>
+            )}
+          </div>
+        </label>
 
-      <label htmlFor="check2">
-        <input
-          id="check2"
-          type="checkbox"
-          onChange={() => setIsChecked2(!isChecked2)}
-        />
-        ¿Estás tomando algún medicamento?
-        <div className="Check-User-Description">
-          {isChecked2 && (
-            <textarea
-              id="TextCheck2"
-              className="TextCheck"
-              placeholder="Indícanos qué medicamentos estás tomando."
-              cols="40"
-              rows="3"
-            ></textarea>
-          )}
-        </div>
-      </label>
+        <label htmlFor="check2">
+          <input
+            id="check2"
+            type="checkbox"
+            onChange={() => setIsChecked2(!isChecked2)}
+          />
+          ¿Estás tomando algún medicamento?
+          <div className="Check-User-Description">
+            {isChecked2 && (
+              <textarea
+                id="TextCheck2"
+                className="TextCheck"
+                placeholder="Indícanos qué medicamentos estás tomando."
+                cols="40"
+                rows="3"
+              ></textarea>
+            )}
+          </div>
+        </label>
 
-      <label htmlFor="check3">
-        <input
-          id="check3"
-          type="checkbox"
-          onChange={() => setIsChecked3(!isChecked3)}
-        />
-        ¿Tienes alguna cirugía previa de nariz, ya sea funcional o estética?
-        <div className="Check-User-Description">
-          {isChecked3 && (
-            <textarea
-              id="TextCheck3"
-              className="TextCheck"
-              placeholder="Indícanos hace cuánto tiempo se realizó la cirugía."
-              cols="40"
-              rows="3"
-            ></textarea>
-          )}
-        </div>
-      </label>
+        <label htmlFor="check3">
+          <input
+            id="check3"
+            type="checkbox"
+            onChange={() => setIsChecked3(!isChecked3)}
+          />
+          ¿Tienes alguna cirugía previa de nariz, ya sea funcional o estética?
+          <div className="Check-User-Description">
+            {isChecked3 && (
+              <textarea
+                id="TextCheck3"
+                className="TextCheck"
+                placeholder="Indícanos hace cuánto tiempo se realizó la cirugía."
+                cols="40"
+                rows="3"
+              ></textarea>
+            )}
+          </div>
+        </label>
 
-      <label htmlFor="check4">
-        <input
-          id="check4"
-          type="checkbox"
-          onChange={() => setIsChecked4(!isChecked4)}
-        />
-        ¿Te hiciste colocar biopolímeros (ácido Hialurónico) o hilos tensores?
-        <div className="Check-User-Description">
-          {isChecked4 && (
-            <textarea
-              id="TextCheck4"
-              className="TextCheck"
-              placeholder="Indícanos hace cuánto tiempo."
-              cols="40"
-              rows="3"
-            ></textarea>
-          )}
-        </div>
-      </label>
+        <label htmlFor="check4">
+          <input
+            id="check4"
+            type="checkbox"
+            onChange={() => setIsChecked4(!isChecked4)}
+          />
+          ¿Te hiciste colocar biopolímeros (ácido Hialurónico) o hilos tensores?
+          <div className="Check-User-Description">
+            {isChecked4 && (
+              <textarea
+                id="TextCheck4"
+                className="TextCheck"
+                placeholder="Indícanos hace cuánto tiempo."
+                cols="40"
+                rows="3"
+              ></textarea>
+            )}
+          </div>
+        </label>
 
-      <label htmlFor="check5">
-        <input
-          id="check5"
-          type="checkbox"
-          onChange={() => setIsChecked5(!isChecked5)}
-        />
-        ¿Tienes dificultad para respirar por la nariz?
-        <div className="Check-User-Description">
-          {isChecked5 && (
-            <textarea
-              id="TextCheck5"
-              className="TextCheck"
-              placeholder="Indícanos de cuál de las 2 fosas nasales."
-              cols="40"
-              rows="3"
-            ></textarea>
-          )}
-        </div>
-      </label>
+        <label htmlFor="check5">
+          <input
+            id="check5"
+            type="checkbox"
+            onChange={() => setIsChecked5(!isChecked5)}
+          />
+          ¿Tienes dificultad para respirar por la nariz?
+          <div className="Check-User-Description">
+            {isChecked5 && (
+              <textarea
+                id="TextCheck5"
+                className="TextCheck"
+                placeholder="Indícanos de cuál de las 2 fosas nasales."
+                cols="40"
+                rows="3"
+              ></textarea>
+            )}
+          </div>
+        </label>
+      </div>
 
       <p>¿Qué es lo que más te molesta de tu nariz?</p>
       <textarea
